c1f9f177eb721b0dacbb7f4f7b668073cfa9a20c
Leon <fluentoxdev17@gmail.com>
2025-04-30 13:46:10 +0200

    Person class

--- CBS-LF8-Verwaltung/src/App.java
+++ CBS-LF8-Verwaltung/src/App.java
@@ -1,4 +1,13 @@
 public class App {
+
+    enum Departments {
+        VERWALTUNG,
+        MARKETING,
+        BUCHHALTUNG,
+        IT,
+        REINIGUNG
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ CBS-LF8-Verwaltung/src/Person.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public abstract class Person {
+    protected String lastName;
+    protected String firstName;
+    protected String address;
+    protected String dateOfBirth;
+
+    private static ArrayList<Person> personList = new ArrayList<>();
+
+    public Person(String lastName, String firstName, String address, String dateOfBirth) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        personList.add(this);
+    }
+
+    public Person() {
+        personList.add(this);
+    }
+
+    public abstract void showInfo();
+
+    public static int getNumberOfPersons() {
+        return personList.size();
+    }
+
+    public static void printPersons() {
+        for (Person p : personList) {
+            p.showInfo();
+        }
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
