8df15b553bd7568984b0fec4174485e639f8ded8
Leon <fluentoxdev17@gmail.com>
2025-05-05 11:49:57 +0200

    test2

--- CBS-LF8-Verwaltung/src/App.java
+++ CBS-LF8-Verwaltung/src/App.java
@@ -8,7 +8,7 @@ public class App {
         REINIGUNG
     }
 
-    // test
+    // test2
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
