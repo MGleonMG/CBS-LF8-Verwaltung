5eb63179c8093546f605f1d6aa0e496a17c6cfda
Leon <fluentoxdev17@gmail.com>
2025-05-06 16:32:40 +0200

    enum Abteilung verschoben

--- CBS-LF8-Verwaltung/src/verwaltung/main/Verwaltung.java
+++ CBS-LF8-Verwaltung/src/verwaltung/main/Verwaltung.java
@@ -1,13 +1,5 @@
 package verwaltung.main;
 
-enum Abteilung {
-    VERWALTUNG,
-    MARKETING,
-    BUCHHALTUNG,
-    IT,
-    REINIGUNG
-}
-
 public class Verwaltung {
 
     public static void main(String[] args) throws Exception {
