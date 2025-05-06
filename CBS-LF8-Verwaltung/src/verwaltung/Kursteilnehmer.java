6a527dd95eadb2dd0ef656f6feded6682c1db935
Leon <fluentoxdev17@gmail.com>
2025-05-06 08:58:43 +0200

    code formatting (2)

--- CBS-LF8-Verwaltung/src/verwaltung/Kursteilnehmer.java
+++ CBS-LF8-Verwaltung/src/verwaltung/Kursteilnehmer.java
@@ -1,13 +1,13 @@
 package verwaltung;
+
 import java.time.LocalDate;
 
 public final class Kursteilnehmer {
-    
+
     private final int MAX_KURSE = 2;
     private static int anzahlKursteilnehmer = 0;
     private int anzahlGebuchterKurse = 0;
     private Kurs[] kurse = new Kurs[MAX_KURSE];
-
     private String name;
     private String vorname;
     private String adresse;
@@ -15,14 +15,14 @@ public final class Kursteilnehmer {
 
     private String placeholder;
 
-    public Kursteilnehmer(String pName, String pVorname, String pAdresse, LocalDate pGeburtsdatum){
+    public Kursteilnehmer(String pName, String pVorname, String pAdresse, LocalDate pGeburtsdatum) {
         name = pName;
         vorname = pVorname;
         adresse = pAdresse;
         geburtsdatum = pGeburtsdatum;
     }
 
-    public Kursteilnehmer(String pName, String pVorname, String pAdresse, LocalDate pGeburtsdatum, Kurs pKurse){
+    public Kursteilnehmer(String pName, String pVorname, String pAdresse, LocalDate pGeburtsdatum, Kurs pKurse) {
         name = pName;
         vorname = pVorname;
         adresse = pAdresse;
@@ -35,20 +35,19 @@ public final class Kursteilnehmer {
         }
     }
 
-    public void bucheKurs(){
+    public void bucheKurs() {
 
     }
 
-    public void storniereKurs(){
+    public void storniereKurs() {
 
     }
 
-    public void zeigeKurse(){
+    public void zeigeKurse() {
 
     }
 
-    public String zeigeInfos(){
+    public String zeigeInfos() {
         return placeholder;
     }
 }
-
