237d931433bf5973bd85a7d71b3670aec2e9e1d5
Ewin <ewinl21@gmail.com>
2025-05-06 11:02:56 +0200

    Code

---
+++ CBS-LF8-Verwaltung/src/verwaltung/Interface/Rabattfaehig.java
@@ -0,0 +1,11 @@
+package verwaltung.Interface;
+
+public interface Rabattfaehig {
+
+    public static void applyRabatt(double prozent){
+        System.out.println("Ein Rabatt von " + prozent + "% wurde angewendet.");
+    }
+
+}
+
+
