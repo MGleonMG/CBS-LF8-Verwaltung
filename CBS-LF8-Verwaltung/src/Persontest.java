1ff18c8d8b9fd2b2039f05805b980985a03d63a5
= <=>
2025-05-12 11:52:29 +0200

    hh

---
+++ CBS-LF8-Verwaltung/src/Persontest.java
@@ -0,0 +1,12 @@
+import javax.print.DocFlavor.STRING;
+
+public abstract class Persontest {
+    STRING name;
+    STRING vorname;
+    STRING adresse;
+    STRING geburtsdatum;
+
+    static ArrayList<persontest> personList = new ArrayList<>();
+
+    
+}
